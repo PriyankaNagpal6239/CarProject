c48f2dc0ce718625176b3e77fa5dcbf38bf394c9
Pawan <pawankumar623916@gmail.com>
2024-07-30 20:40:52 +0530

    Initial commit

--- src/main/java/com/example/car_project/CarProjectApplication.java
+++ src/main/java/com/example/car_project/CarProjectApplication.java
@@ -8,7 +8,7 @@ public class CarProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarProjectApplication.class, args);
-		System.out.println("Hello Running project");
+		System.out.println("Hello Running project is fine");
 	}
 
 }
